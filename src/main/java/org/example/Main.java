a83046bd174433c41a1d33858fbc48ca128780a2
Ro1dex <sanhcez58@gmail.com>
2023-02-08 18:19:23 +0200

    Com

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+public class Main {
+    public static void main(String[] args) {
+        int spend = 250;
+        int oneMilesForSpend = 20;
+        int miles = (spend / oneMilesForSpend);
+        System.out.println("Вам начислено " + miles + " миль.");
+    }
+}
